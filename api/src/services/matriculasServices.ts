f2b10b908833c609135b7ecabbb1228ac7a65bfa
DanielFsR <danielferreirasantosd12@gmail.com>
2024-07-24 20:24:52 -0300

    feat(#7): implement services

---
+++ api/src/services/matriculasServices.ts
@@ -0,0 +1,55 @@
+import { MatriculasRepository } from "../repositories/matriculasRepository";
+
+export type MatriculasInput = {
+  municipio: string;
+  etapa: string;
+};
+
+type Output = {
+  series: {
+    name: string;
+    data: number[];
+  }[];
+  categories: string[];
+};
+
+export class MatriculasService {
+  constructor(private matriculasRepository: MatriculasRepository) {}
+
+  async execute(input: MatriculasInput): Promise<Output> {
+    const data = await this.matriculasRepository.fetch(input);
+
+    if (!data || data.length === 0) {
+      throw new Error('No data found for the given input');
+    }
+
+    const newData: { [key: string]: number[] } = {
+      pretos: [],
+      brancos: [],
+    };
+    const categories: string[] = [];
+
+    data.forEach((entry) => {
+      const category = `${entry.ano} ${entry.rede}`;
+      if (!categories.includes(category)) {
+        categories.push(category);
+      }
+
+      if (entry.raca === 'Pretos') {
+        newData.pretos.push(entry.matricula);
+      } else if (entry.raca === 'Brancos') {
+        newData.brancos.push(entry.matricula);
+      }
+    });
+
+    const series = Object.entries(newData).map(([name, data]) => ({
+      name,
+      data,
+    }));
+
+    return {
+      series,
+      categories,
+    };
+  }
+}
