baf217c4e4d14bcc38e443ef2cfbe77b6b3ced09
DanielFsR <danielferreirasantosd12@gmail.com>
2024-07-24 20:24:18 -0300

    feat(#7): implement repositories

---
+++ api/src/repositories/matriculasRepository.ts
@@ -0,0 +1,14 @@
+import { MatriculasInput } from "../services/matriculasServices";
+
+export type MatriculasRepositoryOutput = {
+    ano: number;
+    raca: string;
+    rede: string;
+    etapa: string;
+    matricula: number;
+    municipio: string;
+}
+
+export abstract class MatriculasRepository {
+    abstract fetch(input: MatriculasInput): Promise<MatriculasRepositoryOutput[]>;
+}
