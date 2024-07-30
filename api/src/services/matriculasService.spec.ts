470d812023a4f088e552098772dce66307f981b6
DanielFsR <danielferreirasantosd12@gmail.com>
2024-07-30 12:07:17 -0300

    feat(#18): Implement tests for sorting and missing data

--- api/src/services/matriculasService.spec.ts
+++ api/src/services/matriculasService.spec.ts
@@ -1,4 +1,10 @@
 import { describe, expect, test } from 'vitest';
+import {
+  disorderedData,
+  genericData,
+  incompleteData,
+  perfectData,
+} from '../database/memory/data/matriculasMemoryData';
 import { MatriculasRepositoryMemory } from '../database/memory/matriculasRepositoryMemory';
 import { MatriculasInput, MatriculasService } from './matriculasServices';
 
@@ -6,56 +12,7 @@ describe('Matriculas Service', () => {
   const matriculaRepository = new MatriculasRepositoryMemory();
   const matriculasService = new MatriculasService(matriculaRepository);
 
-  matriculaRepository.DATA_IN_MEMORY = [
-    {
-      ano: 2020,
-      etapa: 'Ensino Fundamental 1',
-      matricula: 23,
-      municipio: 'Patos de Minas',
-      raca: 'Brancos',
-      rede: 'Pública',
-    },
-    {
-      ano: 2020,
-      etapa: 'Ensino Médio',
-      matricula: 45,
-      municipio: 'Uberaba',
-      raca: 'Brancos',
-      rede: 'Privada',
-    },
-    {
-      ano: 2020,
-      etapa: 'Ensino Médio',
-      matricula: 12,
-      municipio: 'Patos de Minas',
-      raca: 'Brancos',
-      rede: 'Pública',
-    },
-    {
-      ano: 2020,
-      etapa: 'Ensino Médio',
-      matricula: 15,
-      municipio: 'Patos de Minas',
-      raca: 'Brancos',
-      rede: 'Privada',
-    },
-    {
-      ano: 2020,
-      etapa: 'Ensino Médio',
-      matricula: 30,
-      municipio: 'Patos de Minas',
-      raca: 'Pretos',
-      rede: 'Pública',
-    },
-    {
-      ano: 2020,
-      etapa: 'Ensino Médio',
-      matricula: 3,
-      municipio: 'Patos de Minas',
-      raca: 'Pretos',
-      rede: 'Privada',
-    },
-  ];
+  matriculaRepository.DATA_IN_MEMORY = genericData;
 
   const input: MatriculasInput = {
     etapa: 'Ensino Médio',
@@ -106,7 +63,7 @@ describe('Matriculas Service', () => {
     expect(output.categories).toHaveLength(1);
     expect(output.categories[0]).toEqual('2020 Privada');
     expect(output.series[0].name).toEqual('pretos');
-    expect(output.series[0].data).toHaveLength(0);
+    expect(output.series[0].data).toHaveLength(1);
     expect(output.series[1].name).toEqual('brancos');
     expect(output.series[1].data).toHaveLength(1);
     expect(output.series[1].data[0]).toEqual(45);
@@ -120,7 +77,7 @@ describe('Matriculas Service', () => {
     expect(output.categories).toHaveLength(1);
     expect(output.categories[0]).toEqual('2020 Pública');
     expect(output.series[0].name).toEqual('pretos');
-    expect(output.series[0].data).toHaveLength(0);
+    expect(output.series[0].data).toHaveLength(1);
     expect(output.series[1].name).toEqual('brancos');
     expect(output.series[1].data).toHaveLength(1);
     expect(output.series[1].data[0]).toEqual(23);
@@ -140,5 +97,55 @@ describe('Matriculas Service', () => {
     await expect(matriculasService.execute(incorrectMunicipioInput)).rejects.toThrowError(
       'Nenhum dado foi encontrado para estes filtros.',
     );
+  });
+  test('should return data in order', async () => {
+    matriculaRepository.DATA_IN_MEMORY = disorderedData;
+    const response = await matriculasService.execute({
+      ...input,
+      municipio: 'Belo Horizonte',
+    });
+    expect(response.series[0].name).toEqual('pretos');
+    expect(response.series[0].data).toEqual([40, 28, 25, 18]);
+    expect(response.series[1].name).toEqual('brancos');
+    expect(response.series[1].data).toEqual([30, 22, 35, 45]);
+    expect(response.categories).toEqual([
+      '2021 Pública',
+      '2021 Privada',
+      '2022 Pública',
+      '2022 Privada',
+    ]);
+  });
+  test('should return completed data', async () => {
+    matriculaRepository.DATA_IN_MEMORY = incompleteData;
+    const response = await matriculasService.execute({
+      ...input,
+      municipio: 'Belo Horizonte',
+    });
+    expect(response.series[0].name).toEqual('pretos');
+    expect(response.series[0].data).toEqual([25, 0]);
+    expect(response.series[1].name).toEqual('brancos');
+    expect(response.series[1].data).toEqual([35, 45]);
+    expect(response.categories).toEqual(['2022 Pública', '2022 Privada']);
+  });
+  test('should return all data correct', async () => {
+    matriculaRepository.DATA_IN_MEMORY = perfectData;
+    const response = await matriculasService.execute({
+      ...input,
+      municipio: 'Belo Horizonte',
+    });
+    expect(response.series[0].name).toEqual('pretos');
+    expect(response.series[0].data).toEqual([7, 14, 21, 28, 35, 42, 49, 56]);
+    expect(response.series[1].name).toEqual('brancos');
+    expect(response.series[1].data).toEqual([10, 20, 30, 40, 50, 60, 70, 80]);
+    expect(response.categories).toEqual([
+      '2020 Pública',
+      '2020 Privada',
+      '2021 Pública',
+      '2021 Privada',
+      '2022 Pública',
+      '2022 Privada',
+      '2023 Pública',
+      '2023 Privada',
+    ]);
   });
 });
