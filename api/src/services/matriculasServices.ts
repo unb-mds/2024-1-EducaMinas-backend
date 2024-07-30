7e4b7f51f39f48609cc63876ea7c2526547c36ec
DanielFsR <danielferreirasantosd12@gmail.com>
2024-07-30 12:05:42 -0300

    feat(#18): Implement sorting and complete missing data

--- api/src/services/matriculasServices.ts
+++ api/src/services/matriculasServices.ts
@@ -29,18 +29,41 @@ export class MatriculasService {
     };
     const categories: string[] = [];
 
-    data.forEach((entry) => {
-      const category = `${entry.ano} ${entry.rede}`;
-      if (!categories.includes(category)) {
+    // Definindo os anos e redes esperados
+    const anos = [2020, 2021, 2022, 2023];
+    const redes = ['Pública', 'Privada'];
+
+    // Criando combinações de ano e rede
+    anos.forEach((ano) => {
+      redes.forEach((rede) => {
+        const category = `${ano} ${rede}`;
         categories.push(category);
+        newData.pretos.push(0);
+        newData.brancos.push(0);
+      });
+    });
+
+    // Atualizando dados de matrículas
+    data.forEach((entry) => {
+      if (entry.etapa !== input.etapa || entry.municipio !== input.municipio) {
+        throw new Error('Etapa ou municipio errados.');
       }
+      const category = `${entry.ano} ${entry.rede}`;
+      const index = categories.indexOf(category);
 
       if (entry.raca === 'Pretos') {
-        newData.pretos.push(entry.matricula);
+        newData.pretos[index] += entry.matricula;
       } else if (entry.raca === 'Brancos') {
-        newData.brancos.push(entry.matricula);
+        newData.brancos[index] += entry.matricula;
       }
     });
+    for (let i = categories.length - 1; i >= 0; i--) {
+      if (newData.pretos[i] === 0 && newData.brancos[i] === 0) {
+        newData.pretos.splice(i, 1);
+        newData.brancos.splice(i, 1);
+        categories.splice(i, 1);
+      }
+    }
 
     const series = Object.entries(newData).map(([name, data]) => ({
       name,
