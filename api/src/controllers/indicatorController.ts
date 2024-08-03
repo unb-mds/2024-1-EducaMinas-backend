fb8f4a94c76cd974bafafad293a4ca050e42da09
DanielFsR <danielferreirasantosd12@gmail.com>
2024-08-03 10:24:30 -0300

    implement static return in indicatorController

--- api/src/controllers/indicatorController.ts
+++ api/src/controllers/indicatorController.ts
@@ -5,5 +5,19 @@ export const IndicatorController = async (req: Request, res: Response) => {
   console.log(
     `Município: ${municipio} - Etapa: ${etapa} - Indicador: ${indicador} - Rede: ${rede}`,
   );
-  res.json({ municipio, etapa, indicador, rede });
+
+  const response = {
+    categories: [2019, 2020, 2021, 2022, 2023],
+    series: [
+      {
+        name: 'Brancos',
+        data: [12, 34, 14, 29, 45],
+      },
+      {
+        name: 'Pretos',
+        data: [20, 22, 23, 19, 30],
+      },
+    ],
+  };
+  res.json(response);
 };
